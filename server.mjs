d581cf4536df15e2c2f87eea869e0b99316a5de9
Ahn <ggavi2000@naver.com>
2024-07-07 18:50:06 +0900

    catch

--- server.mjs
+++ server.mjs
@@ -9,19 +9,23 @@ const app = next({ hostname, port })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer({
-    key: fs.readFileSync('/etc/letsencrypt/live/asmbedcoding.kro.kr/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/asmbedcoding.kro.kr/cert.pem'),
-  }, async (req, res) => {
-    try {
-        await handle(req, res);
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
-    }
-  })
-    .listen(port, () => {
-      console.log(`> Ready on https://${hostname}:${port}`)
+  try {
+    createServer({
+      key: fs.readFileSync('/etc/letsencrypt/live/asmbedcoding.kro.kr/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/asmbedcoding.kro.kr/cert.pem'),
+    }, async (req, res) => {
+      try {
+          await handle(req, res);
+      } catch (err) {
+        console.error('Error occurred handling', req.url, err)
+        res.statusCode = 500
+        res.end('internal server error')
+      }
     })
+      .listen(port, () => {
+        console.log(`> Ready on https://${hostname}:${port}`)
+      })
+  } catch (error) {
+    console.log(error);
+  }
 })
